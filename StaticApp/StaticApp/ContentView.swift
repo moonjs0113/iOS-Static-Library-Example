721431643fd11fd7ed836cc0b1d3c27dca01c0ab
moonjs0113 <littlemoom@naver.com>
2022-06-29 13:17:27 +0900

    Static Library

---
+++ StaticApp/StaticApp/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  StaticApp
+//
+//  Created by Moon Jongseek on 2022/06/29.
+//
+
+import SwiftUI
+import StaticLibrary
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Text("Hello, world!")
+            CustomButton {
+                print("Custom Button in Library")
+            } label: {
+                Text("Static Library Button")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StaticApp/StaticApp/StaticAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticAppApp.swift
+//  StaticApp
+//
+//  Created by Moon Jongseek on 2022/06/29.
+//
+
+import SwiftUI
+
+@main
+struct StaticAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
